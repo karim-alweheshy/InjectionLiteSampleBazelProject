ba697aeae60a45a7eb189476317c36d392502ac8
Karim Alweheshy <karim.alweheshy@reddit.com>
2025-07-30 20:19:40 +0200

    Initial Commit

---
+++ InjectionLiteSampleBazelProject/InjectionLiteSampleBazelProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InjectionLiteSampleBazelProjectApp.swift
+//  InjectionLiteSampleBazelProject
+//
+//  Created by Karim Alweheshy on 30.07.25.
+//
+
+import SwiftUI
+
+@main
+struct InjectionLiteSampleBazelProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
